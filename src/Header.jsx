4f2288e36a91b7fbd3d54918a2717903152d695d
demirmesutoztas <demiroztas@gmail.com>
2024-05-14 00:11:45 +0300

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './css/Header.css'
+
+function Header() {
+    return (
+        <div>
+            <div className='header'>
+                <div className='title'>UDEMY KURSLAR</div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
